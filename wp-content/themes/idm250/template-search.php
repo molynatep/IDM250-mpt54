75b64dfca5f6539deb6ec791a5afdf0dd35a884a
molynatep <71908029+molynatep@users.noreply.github.com>
2022-02-06 19:04:52 -0500

    search

--- wp-content/themes/idm250/template-search.php
+++ wp-content/themes/idm250/template-search.php
@@ -1,15 +1,49 @@
+<?php
+/* Template Name: Search */
+?>
 <?php get_header(); ?>
 <main>
-    <p>hello</p>
-<?php while (have_posts()) : the_post(); ?>
-<div class="">
-    <h1 class=""><?php the_title(); ?></h1>
-
-    <div class="">
-        <?php the_content(); ?>
-</div>
-</div>
-<?php endwhile; ?>
+    <div class="search">
+        <input class="searchbar" type="text" placeholder="SEARCH">
+    </div>
+    <div class="results">
+    <p>Viewing 3 results for HTML</p>
+    </div>
+    <div class="lineresults">
+    <svg version="1.1" id="line_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="5px" xml:space="preserve">
+    <path class="path2" fill="#01a09e" stroke-width="3" stroke="#ACACBA" d="M0 0 l1120 0"/>
+    </svg>
+    </div>
+    <div class="hometext">
+    <a href="https://molynatep.com/idm231-mpt54/" target="_blank">
+            <div class= "projgrid">
+                <div class="zodiac">
+                    <picture>
+                        <img class="proj" src="/wp-content/themes/idm250/dist/img/zodiac.png">
+                    </picture>
+                </div>
+                <div class="projdesc">
+                    <h3 class="projtitle">PLANT ZODIAC</h3>
+                    <p class="ita">HTML, CSS, Javascript</p>
+                    <p class="projinfo">Coded a plant zodiac desktop webpage using Javascript.(Desktop only)</p>
+                </div>
+                </div>
+                </a>
+            <a href="https://molynatep.com/idm241/final/casestudy/" target="_blank">
+                <div class= "projgrid micro">
+                    <div class="zodiac">
+                        <picture>
+                            <img class="proj" src="/wp-content/themes/idm250/dist/img/microinteractions.png">
+                        </picture>
+                    </div>
+                    <div class="projdesc">
+                        <h3 class="projtitle microtitle">MICROINTERACTIONS</h3>
+                        <p class="ita"> HTML, CSS, Javascript</p>
+                        <p class="projinfo">Designed new microinteractions based off of the Yelp website.</p>
+                    </div>
+                    </div>
+                    </a>
+                </div>
 </main>
 
 
