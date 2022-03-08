c10c41bb46ad5e3edd1d300c733b846767662c36
molynatep <71908029+molynatep@users.noreply.github.com>
2022-03-07 19:34:46 -0500

    favicon lol

--- wp-content/themes/idm250/header.php
+++ wp-content/themes/idm250/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(); ?> </title>
+    <link rel="icon" type="image/x-icon" href="https://molynatep.com/IDM250/wp-content/themes/idm250/dist/img/logo.svg">
     <?php
     wp_head();
     ?>
