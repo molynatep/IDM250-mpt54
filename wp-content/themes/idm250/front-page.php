592f61683ad534161c8d90a2e28ab1ddaf25c3aa
molynatep <71908029+molynatep@users.noreply.github.com>
2022-03-09 12:44:51 -0500

    responsive listing page lols

--- wp-content/themes/idm250/front-page.php
+++ wp-content/themes/idm250/front-page.php
@@ -33,35 +33,6 @@ setup_postdata($post); ?>
     // Reset the global post object so that the rest of the page works correctly.
     wp_reset_postdata(); ?>
 <?php endif; ?>
-
-        <a href="https://molynatep.com/idm231-mpt54/" target="_blank">
-            <div class= "projgrid">
-                <div class="zodiac">
-                    <picture>
-                        <img class="proj" src="wp-content/themes/idm250/dist/img/zodiac.png">
-                    </picture>
-                </div>
-                <div class="projdesc">
-                    <h3 class="projtitle">PLANT ZODIAC</h3>
-                    <p class="ita">HTML, CSS, Javascript</p>
-                    <p class="projinfo">Coded a plant zodiac desktop webpage using Javascript.(Desktop only)</p>
-                </div>
-                </div>
-                </a>
-            <a href="https://molynatep.com/idm241/final/casestudy/" target="_blank">
-                <div class= "projgrid micro">
-                    <div class="zodiac">
-                        <picture>
-                            <img class="proj" src="wp-content/themes/idm250/dist/img/microinteractions.png">
-                        </picture>
-                    </div>
-                    <div class="projdesc">
-                        <h3 class="projtitle microtitle">MICROINTERACTIONS</h3>
-                        <p class="ita"> HTML, CSS, Javascript</p>
-                        <p class="projinfo">Designed new microinteractions based off of the Yelp website.</p>
-                    </div>
-                    </div>
-                    </a>
 </div>
 <a href="http://molynatep.com/IDM250/portfolio-listing/"><h2 class="all">VIEW ALL ></h2></a>
 </main>
--- wp-content/themes/idm250/template-portfolio-listing.php
+++ wp-content/themes/idm250/template-portfolio-listing.php
@@ -5,56 +5,27 @@ Template Name: Portfolio Listing
 ?>
 
 <?php get_header(); ?>
+
 <main>
-    <div class="workshero">
-        <h1 class=""><?php the_title(); ?></h1>
-    </div>
-    <div class="hometext">
-        <a href="http://molynatep.com/IDM250/petes-little-lunch-box/" target="_blank">
-            <div class= "projgrid foodtruck">
-                <div class="zodiac">
-                    <picture>
-                        <img class="proj" src="https://molynatep.com/IDM250/wp-content/themes/idm250/dist/img/lunchboxhero.png">
-                    </picture>
-                </div>
-                <div class="projdesc">
-                    <h3 class="projtitle">FOOD TRUCK APP</h3>
-                    <p class="ita">UX/UI Design</p>
-                    <p class="projinfo">Created a high-fidelity app prototype based on user research.</p>
-                </div>
-                </div>
-                </a>
-        <a href="https://molynatep.com/idm231-mpt54/" target="_blank">
-            <div class= "projgrid">
-                <div class="zodiac">
-                    <picture>
-                        <img class="proj" src=" https://molynatep.com/IDM250/wp-content/themes/idm250/dist/img/zodiac.png">
-                    </picture>
-                </div>
-                <div class="projdesc">
-                    <h3 class="projtitle">PLANT ZODIAC</h3>
-                    <p class="ita">HTML, CSS, Javascript</p>
-                    <p class="projinfo">Coded a plant zodiac desktop webpage using Javascript.(Desktop only)</p>
-                </div>
-                </div>
-                </a>
-            <a href="https://molynatep.com/idm241/final/casestudy/" target="_blank">
-                <div class= "projgrid micro">
-                    <div class="zodiac">
-                        <picture>
-                            <img class="proj" src="https://molynatep.com/IDM250/wp-content/themes/idm250/dist/img/microinteractions.png">
-                        </picture>
-                    </div>
-                    <div class="projdesc">
-                        <h3 class="projtitle microtitle">MICROINTERACTIONS</h3>
-                        <p class="ita"> HTML, CSS, Javascript</p>
-                        <p class="projinfo">Designed new microinteractions based off of the Yelp website.</p>
-                    </div>
-                    </div>
-                    </a>
+<div class="workshero">
+<h1 class=""><?php the_title(); ?></h1>
+</div>
+        <?php
+$featured_posts = get_field('featured_posts');
+if( $featured_posts ): ?>
+    <ul>
+    <?php foreach( $featured_posts as $post ): 
+
+// Setup this post for WP functions (variable must be named $post).
+setup_postdata($post); ?>
+<?php get_template_part('components/project-teaser'); ?>
+    <?php endforeach; ?>
+    </ul>
+    <?php 
+    // Reset the global post object so that the rest of the page works correctly.
+    wp_reset_postdata(); ?>
+<?php endif; ?>
 </div>
 </main>
-
-
 
 <?php get_footer();
