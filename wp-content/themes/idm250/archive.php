ddc0d29341ba9bea3bbbcea4e8120a60a908f256
molynatep <71908029+molynatep@users.noreply.github.com>
2022-03-17 00:29:06 -0400

    archiveeeeeeee

--- wp-content/themes/idm250/archive.php
+++ wp-content/themes/idm250/archive.php
@@ -1,1 +1,15 @@
 <?php get_header(); ?>
+
+<main>
+<div class="workshero">
+<h1 class=""><?php the_archive_title(); ?></h1>
+</div>
+    <?php
+    while (have_posts()) : the_post();
+    get_template_part('components/project-teaser');
+    endwhile;
+    ?>
+</div>
+</main>
+
+<?php get_footer();
