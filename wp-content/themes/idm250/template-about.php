1161d8c0bd3dad8dc3da6b28bad0e97ac8488e33
molynatep <71908029+molynatep@users.noreply.github.com>
2022-03-18 09:36:42 -0400

    abt

---
+++ wp-content/themes/idm250/template-about.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Template Name: About
+*/
+?>
+
+<?php get_header(); ?>
+<main>
+<?php while (have_posts()) : the_post(); ?>
+<div class="">
+<div class="workshero">
+        <h1 class=""><?php the_title(); ?> 
+        </h1>
+        <?php
+    // Get project categories from this post ID
+    $project_categories = get_the_terms(get_the_ID(), 'idm-project-categories');
+if ($project_categories) {
+    // Get total items count
+    $total = count($project_categories) - 1;
+    // Loop through this post's categories and spit them out
+    foreach ($project_categories as $key => $category) {
+        $category_link = get_term_link($category->term_id);
+        echo "<span class='project-teaser__tag'><a href='$category_link'>$category->name</a></span>";
+        // Add comma after every loop. Let's not add it to the last item in the loop
+        if ($key != $total) {
+            echo ', ';
+            }
+        }
+    }
+    ?>
+    </div>
+    <div class="container sidebar">
+      <aside class="sidebar-aside">
+    <?php dynamic_sidebar('sidebar-primary'); ?>
+  </aside>
+</div>
+
+    <div class="portfoliocontent">
+        <div class="contentwidth">
+        <?php the_content(); ?>
+        </div>
+    </div>
+
+<?php endwhile; ?>
+
+</main>
+<?php get_footer();
--- wp-content/themes/idm250/template-portfolio-listing.php
+++ wp-content/themes/idm250/template-portfolio-listing.php
@@ -10,22 +10,13 @@ Template Name: Portfolio Listing
 <div class="workshero">
 <h1 class=""><?php the_title(); ?></h1>
 </div>
-        <?php
-$featured_posts = get_field('featured_posts');
-if( $featured_posts ): ?>
-    <ul>
-    <?php foreach( $featured_posts as $post ): 
-
-// Setup this post for WP functions (variable must be named $post).
-setup_postdata($post); ?>
-<?php get_template_part('components/project-teaser'); ?>
-    <?php endforeach; ?>
-    </ul>
-    <?php 
-    // Reset the global post object so that the rest of the page works correctly.
-    wp_reset_postdata(); ?>
-<?php endif; ?>
+<div class="portfoliocontent">
+        <div class="contentwidth">
+        <?php the_content(); ?>
+        </div>
+    </div>
 </div>
+<?php endwhile; ?>
 </main>
 
 <?php get_footer();
